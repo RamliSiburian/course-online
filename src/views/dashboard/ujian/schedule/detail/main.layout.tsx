5cdc45cb2461d966b4c514adf900b8226831dc34
Ramli Siburian <ramlisiburian1997@gmail.com>
2024-05-05 21:56:06 +0700

    update

--- src/views/dashboard/ujian/schedule/detail/main.layout.tsx
+++ src/views/dashboard/ujian/schedule/detail/main.layout.tsx
@@ -4,7 +4,7 @@ import { ModalConfirm } from '@afx/components/common/modals/modal-confirm.layout
 import LynxStorages from '@afx/utils/storage.util';
 import { IActionExamSchedule, IStateExamSchedule } from '@lynx/models/exam/client/schedule.model';
 import { useLynxStore } from '@lynx/store/core';
-import { Col, Image, Row } from 'antd';
+import { Col, Image, Row, Skeleton, Space } from 'antd';
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Order from '@lynx/images/order.png'
@@ -19,13 +19,14 @@ import LynxCurrency from '@afx/components/common/typography/currency.layout';
 
 export function DetailSchedule(): React.JSX.Element {
     const router = useRouter()
-    const { state, useActions: schedules } = useLynxStore<IStateExamSchedule, IActionExamSchedule>('schedule')
+    const { state, useActions: schedules, isLoading: scheduleloading } = useLynxStore<IStateExamSchedule, IActionExamSchedule>('schedule')
     const { useActions: exam } = useLynxStore<IStateExam, IActionExam>('exam')
     const { useActions: claimExam, isLoading } = useLynxStore<IStatePayment, IActionPayment>('payment')
     const { scheduleID: params }: { scheduleID: string } = useParams() as any
     const [openConfirm, setOpenConfirm] = useState<boolean>(false)
     const roleUser = LynxStorages.getItem('ADZKIA@UROLE', true, true).data[0]
-    const loading = isLoading('claimExam') || false
+    const claimLoading = isLoading('claimExam') || false
+    const loading = scheduleloading('getDetailExam') || scheduleloading('getFormRegister') || false
 
     useEffect(() => {
         schedules<'getDetailExam'>('getDetailExam', [params], true)
@@ -115,13 +116,14 @@ export function DetailSchedule(): React.JSX.Element {
             var paramsToClaim = new FormData()
             paramsToClaim.set('id', state?.detailSchedule?.id)
             paramsToClaim.set('name', state?.detailSchedule?.title)
-            paramsToClaim.set('type', state?.detailSchedule?.type)
+            paramsToClaim.set('type', 'tryout')
             paramsToClaim.set('user_id', roleUser?.user_id)
 
             claimExam<'claimExam'>('claimExam', [paramsToClaim, (status: number) => {
                 if (status === 200) {
                     SuccessNotif({ key: 'CLAIM', message: 'Succes to load data', description: 'Exam has been claimed' })
                     schedules<'getDetailExam'>('getDetailExam', [params], true)
+                    schedules<'getFormRegister'>('getFormRegister', [params], true)
                     setOpenConfirm(false)
                 }
             }], true)
@@ -183,66 +185,94 @@ export function DetailSchedule(): React.JSX.Element {
 
 
     return (
-        <div className='shadow-2xl p-4 h-full' >
+        <div className='shadow-2xl p-8 h-full' >
             <div className='flex items-center gap-4 mb-10'>
                 <Icons onClick={() => router.replace('/page/dashboard/tryout/schedules')} style={{ color: '#2d4379', fontWeight: 'bold' }} type='ArrowLeftOutlined' size={18} />
                 <p className='text-base-color font-bold text-xl'>Detail Ujian</p>
             </div>
-            <Row gutter={[0, 40]}>
-                <Col span={6}>
-                    <Image className='rounded-lg' height={300} alt="example" src="https://klik-adzkia.com/assets/assets/images/no_thumbnail/tryout.jpg" />
-                </Col>
-                <Col span={18}>
-                    <div>
-                        <div className='p-4 rounded-lg shadow-md w-full'>
-                            <p className='text-base-color'>{state?.detailSchedule?.title}</p>
-
-                            <div className='mt-2 text-base-color' >
-                                <Row gutter={[0, 10]} >
-                                    <Col span={6}><p className='font-normal text-xs'>jumlah Soal</p></Col>
-                                    <Col span={18}><p className='font-normal text-xs'>: {state?.formRegister?.total_question}</p></Col>
-                                    <Col span={6}><p className='font-normal text-xs'>Durasi Ujian</p></Col>
-                                    <Col span={18}><p className='font-normal text-xs'>: {state?.formRegister?.duration} Menit</p></Col>
-                                    <Col span={6}><p className='font-normal text-xs'>Kapasitas Peserta</p></Col>
-                                    <Col span={18}><p className='font-normal text-xs'>: {state?.formRegister?.total_registered}/{state?.formRegister?.quota}</p></Col>
-                                </Row>
+            {loading ?
+                <div>
+                    <div className='flex gap-10'>
+                        <Skeleton.Image className='!h-[300px] !w-[300px]' active />
+                        <div className='w-full flex flex-col gap-2'>
+                            <Skeleton.Input active style={{ width: '300px' }} />
+                            <Skeleton.Input block active />
+                            <Skeleton.Input block active />
+                            <Skeleton.Input block active />
+                            <Skeleton.Input block active />
+                            <div className='flex justify-between mt-4'>
+                                <Skeleton.Input active />
+                                <Skeleton.Button style={{ width: '120px' }} active />
                             </div>
                         </div>
-                        <div className='w-full flex justify-between items-center mt-10'>
-                            <div>
-                                <p className='text-base-color'>Biaya Pendaftaran</p>
-                                <p className='text-base-color'>{state?.detailSchedule?.price === null ? 'Gratis' : <LynxCurrency value={state?.detailSchedule?.price} prefix='Rp.' />}</p>
+                    </div>
+
+                    <div className='w-full flex flex-col gap-2 mt-14'>
+                        <Skeleton.Input active style={{ width: '256px' }} />
+                        <Skeleton.Input block active className='!w-3/6' />
+                        <Skeleton.Input block active className='!w-3/5' />
+                        <Skeleton.Input block active className='!w-3/4' />
+                        <Skeleton.Input block active className='!w-2/3 ' />
+                        <Skeleton.Input block active className='!w-3/5' />
+                    </div>
+                </div>
+                :
+                <Row gutter={[0, 40]}>
+                    <Col span={6}>
+                        <Image className='rounded-lg' height={300} alt="example" src="https://klik-adzkia.com/assets/assets/images/no_thumbnail/tryout.jpg" />
+                    </Col>
+                    <Col span={18}>
+                        <div>
+                            <div className='p-4 rounded-lg shadow-md w-full'>
+                                <p className='text-base-color'>{state?.detailSchedule?.title}</p>
+
+                                <div className='mt-2 text-base-color' >
+                                    <Row gutter={[0, 10]} >
+                                        <Col span={6}><p className='font-normal text-xs'>jumlah Soal</p></Col>
+                                        <Col span={18}><p className='font-normal text-xs'>: {state?.formRegister?.total_question}</p></Col>
+                                        <Col span={6}><p className='font-normal text-xs'>Durasi Ujian</p></Col>
+                                        <Col span={18}><p className='font-normal text-xs'>: {state?.formRegister?.duration} Menit</p></Col>
+                                        <Col span={6}><p className='font-normal text-xs'>Kapasitas Peserta</p></Col>
+                                        <Col span={18}><p className='font-normal text-xs'>: {state?.formRegister?.total_registered}/{state?.formRegister?.quota}</p></Col>
+                                    </Row>
+                                </div>
                             </div>
-                            {state?.formRegister?.exam === null ? (
-                                state?.formRegister?.total_registered < state?.formRegister?.quota ? (
-                                    state?.detailSchedule?.price === null ? <LynxButtons onClick={() => setOpenConfirm(true)} title='Ikuti Ujian' className='!w-32' />
-                                        : <LynxButtons title='Ikuti Ujian' className='!w-32' />
-                                ) : <LynxButtons disabled title='Kuota Penuh' className='!w-32 !bg-[#f00]' />
-                            ) : 'aa' //TODO: nect case if exam paid
+                            <div className='w-full flex justify-between items-center mt-10'>
+                                <div>
+                                    <p className='text-base-color'>Biaya Pendaftaran</p>
+                                    <p className='text-base-color'>{state?.detailSchedule?.price === null ? 'Gratis' : <LynxCurrency value={state?.detailSchedule?.price} prefix='Rp.' />}</p>
+                                </div>
+                                {state?.formRegister?.exam === null ? (
+                                    state?.formRegister?.total_registered < state?.formRegister?.quota ? (
+                                        state?.detailSchedule?.price === null ? <LynxButtons onClick={() => setOpenConfirm(true)} title='Ikuti Ujian' className='!w-32' />
+                                            : <LynxButtons title='Ikuti Ujian' className='!w-32' />
+                                    ) : <LynxButtons disabled title='Kuota Penuh' className='!w-32 !bg-[#f00]' />
+                                ) : <LynxButtons onClick={() => { }} title='Mulai Ujian' className='!w-32' />
 
-                            }
+                                }
+                            </div>
                         </div>
-                    </div>
-                </Col>
-                <Col span={24}>
-                    <div>
-                        <p className='text-base-color font-semibold text-base mb-2'>Tata Tertib Ujian</p>
-                        <ol start={1} type='1' className='text-base-color'>
-                            <li>Pastikan Koneksi Internet anda stabil dan paket data anda cukup untuk mengikuti ujian.</li>
-                            <li>Pilihlah jawaban yang paling benar menurut Anda lalu tekan tombol Simpan untuk melanjutkan ke nomor soal berikutnya.</li>
-                            <li>Jika Anda masih ragu-ragu dengan jawaban soal tersebut maka Anda bisa menekan tombol Lewati untuk ke soal berikutnya.</li>
-                            <li>Jika Anda ingin mengerjakan soal secara acak maka Anda bisa menekan tombol , lalu pilih nomor soal yang Anda inginkan dan jangan lupa simpan setiap jawaban yang sudah dipilih.</li>
-                            <li>Jika Anda menekan tombol Selesai maka Anda dianggap sudah menyelesaikan ujian tersebut dan tidak bisa mengulanginya lagi.</li>
-                            <li>Pastikan waktu ujian Anda cukup untuk menyelesaikan seluruh soal. Jika tidak maka ujian akan selesai secara otomatis atau akan masuk ke ujian berikutnya.</li>
-                            <li>Jika seluruh ujian sudah selesai dan Anda sudah menekan tombol Selesai maka Anda akan diarahkan ke menu hasil ujian : skor per bidang, total skor, lulus/tidak lulus, dan rinciannya.</li>
-                            <li>Setelah Anda menekan Tombol Mulai Ujian maka waktu akan otomatis berjalan.</li>
-                        </ol>
-                    </div>
-                </Col>
+                    </Col>
+                    <Col span={24}>
+                        <div>
+                            <p className='text-base-color font-semibold text-base mb-2'>Tata Tertib Ujian</p>
+                            <ol start={1} type='1' className='text-base-color'>
+                                <li>Pastikan Koneksi Internet anda stabil dan paket data anda cukup untuk mengikuti ujian.</li>
+                                <li>Pilihlah jawaban yang paling benar menurut Anda lalu tekan tombol Simpan untuk melanjutkan ke nomor soal berikutnya.</li>
+                                <li>Jika Anda masih ragu-ragu dengan jawaban soal tersebut maka Anda bisa menekan tombol Lewati untuk ke soal berikutnya.</li>
+                                <li>Jika Anda ingin mengerjakan soal secara acak maka Anda bisa menekan tombol , lalu pilih nomor soal yang Anda inginkan dan jangan lupa simpan setiap jawaban yang sudah dipilih.</li>
+                                <li>Jika Anda menekan tombol Selesai maka Anda dianggap sudah menyelesaikan ujian tersebut dan tidak bisa mengulanginya lagi.</li>
+                                <li>Pastikan waktu ujian Anda cukup untuk menyelesaikan seluruh soal. Jika tidak maka ujian akan selesai secara otomatis atau akan masuk ke ujian berikutnya.</li>
+                                <li>Jika seluruh ujian sudah selesai dan Anda sudah menekan tombol Selesai maka Anda akan diarahkan ke menu hasil ujian : skor per bidang, total skor, lulus/tidak lulus, dan rinciannya.</li>
+                                <li>Setelah Anda menekan Tombol Mulai Ujian maka waktu akan otomatis berjalan.</li>
+                            </ol>
+                        </div>
+                    </Col>
 
-                {/* <button onClick={getImage}>GET</button>
+                    {/* <button onClick={getImage}>GET</button>
                 {image && <Image alt='tets' src={image} />} */}
-            </Row>
+                </Row>
+            }
             <ModalConfirm loading={false} description="Claim Produk Gratis" onCancel={() => setOpenConfirm(false)} onSave={freeRegister} open={openConfirm} srcImage={Order} textSave="Claim Now" />
         </div>
     )
